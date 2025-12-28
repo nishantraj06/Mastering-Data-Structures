ce8bf2b598302005956489eb986140e18a851c6e
nishantraj06 <nishantraj135246@gmail.com>
2025-12-28 17:20:52 +0530

    day 6

---
+++ 02-Fast-slow-pointer/Solution.java
@@ -0,0 +1,32 @@
+// Linked List Cycle
+// LEETCODE 141
+// level Easy
+// Time complexity  o(n)
+// space complexity  0(1)
+
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) {
+          val = x;
+         next = null;
+     }
+    }
+    
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        boolean result = false;
+
+        while(fast != null && fast.next != null){
+            fast=fast.next.next;
+            slow=slow.next;
+            if(fast == slow){
+                    result = true;
+                    break;
+            }
+        }
+        return result;
+    }
+}
