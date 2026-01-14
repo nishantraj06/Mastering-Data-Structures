261c7b5458cf344a4c7ed613b8c1c6e661912731
nishantraj06 <nishantraj135246@gmail.com>
2026-01-14 22:58:03 +0530

    day 14

---
+++ 04-Dummy-Node/Problem_01_Leetcode_21.java
@@ -0,0 +1,42 @@
+// Merge Two Sorted Lists
+// LEETCODE  21
+// level Easy
+// Time complexity  o(n)
+// space complexity  0(1)
+ class ListNode{
+      int val;
+      ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
+public class Problem_01_Leetcode_21{
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        
+        ListNode dummy = new ListNode(-1,null);
+        ListNode temp = dummy;
+
+        while(list1 != null && list2 != null){
+            if(list1.val < list2.val){ 
+                temp.next = list1;
+                list1 = list1.next;
+           }else{
+                temp.next=list2;
+                list2 = list2.next;
+            }
+            temp = temp.next;
+        }
+        if(list1 != null){
+            temp.next=list1;
+        }
+        
+        if(list2 != null){
+            temp.next=list2;
+        }
+
+        return dummy.next;
+    }
+}
+     
+  
--- README.md
+++ README.md
@@ -30,6 +30,7 @@ This repository is a dedicated log of my journey to master Problem Solving. I am
  | 11 | Middle node of Linked List  | Easy | Floyd's Tortoise and Hare | ✅ |
  | 12 | Revserve Linked List  | Easy | Linked list | ✅ |
 | 13 |   Delete Node in a Linked List  | Medium | Linked list | ✅ |
-| 14| Rotate List | Medium | Length calculation + Modular rotation | 100% |✅ |
+| 14| Rotate List | Medium | Length calculation + Modular rotation | ✅ |
 |15|Intersection of Two Linked Lists| Medium |Two pointer approach |  ✅ |
 |16|Swapping Nodes in a Linked List|Easy | Floyd's Tortoise and Hare |  ✅ |
+| 17 | Merge Two Sorted Lists | Easy | Dummy node + two pointer |   ✅ |
